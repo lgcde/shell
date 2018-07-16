30e447463dda133547d4075b75c18dd670ffc76e
lgcde <34829026+lgcde@users.noreply.github.com>
2018-07-16 21:26:19 +0800

    Delete termux_mojo.sh

--- termux_mojo.sh
+++
@@ -1,5 +0,0 @@
-pkg install -y curl wget make clang *YAML* perl openssl-dev openssl-tool coreutils termux-tools
-termux-setup-storage
-curl http://share-10066126.cos.myqcloud.com/cpanm.pl|perl - App::cpanminus
-cpanm Mojo::Webqq Mojo::Weixin Webqq::Encryption
-wget -P /storage/emulated/0/ https://github.com/lgcde/shell/raw/master/qq.pl https://github.com/lgcde/shell/raw/master/wx.pl
